d0401987c75cfd9c465bd382c19985ca24a957bd
Swapnil Lakra <swapnillakra822@gmail.com>
2026-06-19 18:54:05 +0530

    chore(ci): validate backend delivery pipeline

--- app/fashion-d2c-app/backend/main.py
+++ app/fashion-d2c-app/backend/main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers import products   # because routers folder is in same directory as main.py
 from database import engine, Base
 import asyncio
-
 from datetime import datetime
 
 async def init_db():
